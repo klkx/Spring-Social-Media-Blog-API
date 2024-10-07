fb868646bdd7b0e13afb18c70442663b41fe42be
Minmin(Kayson) Lin <klkxxy@gmail.com>
2024-10-07 19:30:15 +0000

    Account registration done

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,31 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    private AccountService accountService;
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService){
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity registerAccount(@RequestBody Account account) {
+        if(account.getUsername().length()>0 && account.getPassword().length()>=4){
+            Optional<Account> foundAccount_opt = accountService.findAccountByUsername(account.getUsername());
+            if(foundAccount_opt.isEmpty()){
+                Account savedAccount = accountService.registerAccount(account);
+                return ResponseEntity.status(200).body(savedAccount);
+            }else{
+                return ResponseEntity.status(409).body("Duplicated username.");
+            }
+        }else{
+            return ResponseEntity.status(400).body("Empty username or too short password.");
+        }
+        
+        
+    }
+
+    
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> findAccountByUsername(String username){
+        return accountRepository.findByUsername(username);
+    }
+
 }
